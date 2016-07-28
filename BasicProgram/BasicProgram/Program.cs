1e4e3ddfac67d0f68f70c26ba54a6d9c271e5e67
dotnetcubic <dotnetcubic123@gmail.com>
2016-07-27 20:27:47 -0500

    intial changes

---
+++ BasicProgram/BasicProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample obj = new Sample();
+            int sumResult = obj.Sum((4, 5);
+            Console.WriteLine("Sum result is :");
+            Console.WriteLine(sumResult);
+            Console.Read();
+
+
+
+        }
+    }
+
+    public class Sample
+    {
+
+        public int Sum(int a,int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        public int Sum(int a,int b,int c)
+        {
+            int result = a + b + c;
+            return result;
+        }
+
+    }
+
+
+}
