e524a4ea071e2ad9abbce9fbdc06c51b8f28ecdd
dotnetcubic <dotnetcubic123@gmail.com>
2016-08-02 20:49:11 -0500

    adding one more comment

--- BasicProgram/BasicProgram/Class1.cs
+++ BasicProgram/BasicProgram/Class1.cs
@@ -12,7 +12,6 @@ namespace BasicProgram
         // Hello World 
         //what else can we do?
         // adding comment to see merge issues.
-
-    
+        // adding one more comment
     }
 }
